5bceff974e381cbb21b48a9cf9669522eef9b3f5
Olga Boiaryntseva <olgaboiar@gmail.com>
2019-07-07 17:05:07 -0400

    Refactor passing user info from back end to React

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,14 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from '../components/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../components/App';
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  const node = document.getElementById('current-user')
-  const data = JSON.parse(node.getAttribute('data'))
-  console.log(data)
   ReactDOM.render(
-    <App user={data}/>,
+    <App appState={appState}/>,
     document.body.appendChild(document.createElement('div')),
   )
 })

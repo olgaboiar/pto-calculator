991d820dd3dee11c00a85124f29c2286f6bf9ba4
Olga Boiaryntseva <olgaboiar@gmail.com>
2019-07-07 17:39:11 -0400

    Create employee profile rafter sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create :create_employee_profile
+
+  private
+       
+  def create_employee_profile
+    Employee.create(user_id: self.id) 
+  end
 end

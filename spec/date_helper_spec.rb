9564510f9972de5b318ec1db43b355891befd293
Olga Boiaryntseva <olgaboiar@gmail.com>
2019-07-08 15:23:42 -0400

    Refactor Date helper

--- spec/date_helper_spec.rb
+++ spec/date_helper_spec.rb
@@ -18,16 +18,30 @@ describe DateHelper do
   describe '#get_working_month_amount' do
     it 'should return 1 for starting date Jun 1st' do
       allow(@date_helper).to receive(:get_current_date).and_return Date.new(2019,7,1)
-      starting_date = "2019-06-01T05:00:00.000Z"
+      starting_date = Date.new(2019,6,1)
       actual = @date_helper.get_working_month_amount(starting_date)
       expect(actual).to eq(1)
     end
 
     it 'should return 6 for starting date Jan 1st' do
       allow(@date_helper).to receive(:get_current_date).and_return Date.new(2019,7,1)
-      starting_date = "2019-01-01T05:00:00.000Z"
+      starting_date = Date.new(2019,1,1)
       actual = @date_helper.get_working_month_amount(starting_date)
       expect(actual).to eq(6)
+    end
+
+    it 'should return 1 for starting date Apr 1st and Graduation day May 1st' do
+      starting_date = Date.new(2019,4,1)
+      graduation_date = Date.new(2019,5,1)
+      actual = @date_helper.get_working_month_amount(starting_date, graduation_date)
+      expect(actual).to eq(1)
+    end
+  
+    it 'should return 4 for starting date Jan 21st and Graduation day May 1st' do
+      starting_date = Date.new(2019,1,21)
+      graduation_date = Date.new(2019,5,1)
+      actual = @date_helper.get_working_month_amount(starting_date, graduation_date)
+      expect(actual).to eq(4)
     end
   end
 end

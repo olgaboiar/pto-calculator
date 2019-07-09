d0e53d024683d18aff48b525b4c170491533ff1a
Olga Boiaryntseva <olgaboiar@gmail.com>
2019-07-09 13:51:44 -0400

    Create EmploymentHistory model

---
+++ app/models/employment_history.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class EmploymentHistory < ApplicationRecord
+  belongs_to :user
+end
---
+++ db/migrate/20190709174827_create_employment_histories.rb
@@ -0,0 +1,12 @@
+class CreateEmploymentHistories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :employment_histories do |t|
+      t.integer :user_id
+      t.string :position
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps
+    end
+  end
+end

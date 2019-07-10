f64c2de8ff0cfa2a26d129118a857f43c2a8448c
Olga Boiaryntseva <olgaboiar@gmail.com>
2019-07-10 15:44:44 -0400

    WIP

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1,18 +1,27 @@
 # frozen_string_literal: true
 
 class EmployeeController < ApplicationController
-  def update
+  def profile_update
     employee = Employee.find_by(user_id: params[:user_id])
     employee.update(employee_params)
   end
 
+  def history_update
+    initial = params[:initial_ids]
+    params[:entries].each do |param|
+        if param[:id]
+            initial.delete(param[:id])
+            history_entry  = EmploymentHistory.find(param[:id])
+            history_entry.update(employee_id: params[:employee_id], position: param[:position], start_date: param[:start_date], end_date: param[:end_date])
+        else
+            EmploymentHistory.create(employee_id: params[:employee_id], position: param[:position], start_date: param[:start_date], end_date: param[:end_date])
+        end
+    end
+    users_to_delete = EmploymentHistory.where(id: initial)
+    users_to_delete.delete_all
+  end
+
   def employee_params
-    params.permit(
-      :user_id,
-      :location,
-      :start_date,
-      :current_position,
-      :starting_position,
-      :graduation_date)
+    params.permit(:user_id, :location)
   end
 end

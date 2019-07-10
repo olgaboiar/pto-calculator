450aa149e9fbfd4ba99316a9b6ee2c51568a0d8e
Olga Boiaryntseva <olgaboiar@gmail.com>
2019-07-09 20:31:41 -0400

    Create employment history on user creation

--- app/controllers/pto_calculator_controller.rb
+++ app/controllers/pto_calculator_controller.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class PtoCalculatorController < ApplicationController
-  before_action :crate_calculator
-  before_action :authenticate_user!, only: [:calculate]
+  before_action :create_calculator
+#   before_action :authenticate_user!, only: [:calculate]
 
   def index
     if user_signed_in?
       @user = current_user
       @employee = Employee.find_by(user_id: @user.id)
+      @employment = EmploymentHistory.find_by(user_id: @user.id)
       @pto = @calculator.calculate(@employee.start_date, @employee.graduation_date, @employee.current_position, @employee.starting_position)
     else
       @user = nil
@@ -16,7 +17,7 @@ class PtoCalculatorController < ApplicationController
 
   private
 
-  def crate_calculator
+  def create_calculator
     date_helper = DateHelper.new
     @calculator = Calculator.new(date_helper)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  after_create :create_employee_profile
+  after_create :create_employee_profile, :create_employment_history
 
   private
 
   def create_employee_profile
     Employee.create(user_id: id)
+  end
+
+  def create_employment_history
+    EmploymentHistory.create(user_id: id)
   end
 end

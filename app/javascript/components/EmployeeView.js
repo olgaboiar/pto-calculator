741da5a1368161aa038a05809f621ccfd1d48df7
Olga Boiaryntseva <olgaboiar@gmail.com>
2019-07-11 02:47:34 -0400

    Cleanup Employee view

--- app/javascript/components/EmployeeView.js
+++ app/javascript/components/EmployeeView.js
@@ -18,7 +18,9 @@ class EmployeeView extends React.Component {
           <Col m={1} s={12} offset={"m11"}>
             <Button waves="light" node="a" href="/users/sign_out" data-method="delete" data-testid="logout-link">Logout</Button>
           </Col>
-          <Col m={4} s={12} data-testid="pto">
+        </Row>
+        <Row>
+          <Col m={5} s={12} data-testid="pto">
             <Pto user={this.state.user}/>
           </Col>
           <Col m={7} s={12} data-testid="employment">
